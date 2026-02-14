46b01f709b6cd443c8f4dd6fbb573701b6bd377d
Gabrielpedroaurelio <gabrielpedroaurelio9@gmail.com>
2026-02-14 13:10:44 +0100

    refactore: change string to pages in render of router

--- votaaki/frontend/src/Routers/router.tsx
+++ votaaki/frontend/src/Routers/router.tsx
@@ -1,17 +1,21 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Auth from "../Pages/Auth/Auth";
+import Vote from '../Pages/Vote/Vote'
+import Register from "../Pages/Register/Register";
+import Poll from "../Pages/Poll/Poll";
+import NotFound from "../Pages/NotFound/NotFound";
 
 export default function Routers() {
     return (
         <>
             <BrowserRouter>
                 <Routes>
-                    <Route path="*" element="Not Found"></Route>
-                    <Route path="/" index element="Poll Page"></Route>
-                    <Route path="/poll" element="Poll Page"></Route>
+                    <Route path="*" element={<NotFound/>}></Route>
+                    <Route path="/" index element={<Poll/>}></Route>
+                    <Route path="/poll" element={<Poll/>}></Route>
                     <Route path="/auth" element={<Auth/>}></Route>
-                    <Route path="/register" element="Register Poll"></Route>
-                    <Route path="/vote" element="Vote Page"></Route>
+                    <Route path="/register" element={<Register/>}></Route>
+                    <Route path="/vote" element={<Vote/>}></Route>
                 </Routes>
             </BrowserRouter>
 
