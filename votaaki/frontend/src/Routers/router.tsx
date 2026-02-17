7fc866b230f295261c1eec2e5a1f58433596754c
Gabrielpedroaurelio <gabrielpedroaurelio9@gmail.com>
2026-02-17 08:05:37 +0100

    fix:modifed router target and I lost changes 😭😭😭😭😭😭😭😭😭😭😭😭

--- votaaki/frontend/src/Routers/router.tsx
+++ votaaki/frontend/src/Routers/router.tsx
@@ -1,8 +1,6 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Auth from "../Pages/Auth/Auth";
-import Vote from '../Pages/Vote/Vote'
-import Register from "../Pages/Register/Register";
-import Poll from "../Pages/Poll/Poll";
+
 import NotFound from "../Pages/NotFound/NotFound";
 
 export default function Routers() {
@@ -11,11 +9,11 @@ export default function Routers() {
             <BrowserRouter>
                 <Routes>
                     <Route path="*" element={<NotFound/>}></Route>
-                    <Route path="/" index element={<Poll/>}></Route>
-                    <Route path="/poll" element={<Poll/>}></Route>
+                    <Route path="/" index element={"Poll"}></Route>
+                    <Route path="/poll" element={"Poll"}></Route>
                     <Route path="/auth" element={<Auth/>}></Route>
-                    <Route path="/register" element={<Register/>}></Route>
-                    <Route path="/vote" element={<Vote/>}></Route>
+                    <Route path="/register" element={"Register"}></Route>
+                    <Route path="/vote" element={"Vote"}></Route>
                 </Routes>
             </BrowserRouter>
 
